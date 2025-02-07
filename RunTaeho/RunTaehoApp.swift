e1bb2edc6ef182e1c2582f7f8bff9f25c1f790ce
홍태호 <malamute10@xinapse.ai>
2025-02-07 17:32:33 +0900

    Initial Commit

---
+++ RunTaeho/RunTaehoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunTaehoApp.swift
+//  RunTaeho
+//
+//  Created by Hong Taeho on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct RunTaehoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RunTaehoTests/RunTaehoTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunTaehoTests.swift
+//  RunTaehoTests
+//
+//  Created by Hong Taeho on 2/7/25.
+//
+
+import Testing
+@testable import RunTaeho
+
+struct RunTaehoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
