24991a2b331c7e13e2feccee2e8b7b4c0188616c
홍태호 <malamute10@xinapse.ai>
2025-04-11 15:28:28 +0900

    StatsManager 파일 분리

--- RunTaeho/Features/Running/ViewModels/RunningViewModel.swift
+++ RunTaeho/Features/Running/ViewModels/RunningViewModel.swift
@@ -2,23 +2,6 @@ import Foundation
 import Combine
 import CoreLocation
 
-// 통계 관리 클래스
-class StatsManager {
-    @Published var bpm: Int = 0
-    @Published var pace: (minutes: Int, seconds: Int) = (0, 0)
-    @Published var speed: Double = 0.0
-    
-    func updateStats(distance: Double, elapsedSeconds: Int) {
-        print("distance: \(distance), elapsedSeconds: \(elapsedSeconds)")
-        
-        bpm = Int.random(in: 120...150)
-        let paceSeconds = distance > 0 ? Int((Double(elapsedSeconds) / distance) * 1000) : 0
-        pace.minutes = paceSeconds / 60
-        pace.seconds = paceSeconds % 60
-        speed = (distance / Double(elapsedSeconds)) * 3.6
-    }
-}
-
 // RunningViewModel
 class RunningViewModel: ObservableObject {
     @Published private(set) var runningStatus: eRunningStatus = .Stopped
